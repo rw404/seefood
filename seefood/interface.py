659dc7276e0a82ff25d45b5826fa857b3a72fb8f
alexander <alexsmirnov0101@mail.ru>
2024-05-23 21:32:15 +0300

    add gradio interface

---
+++ seefood/interface.py
@@ -0,0 +1,35 @@
+import time
+
+import gradio as gr
+import numpy as np
+import torch
+from PIL import Image
+from .model import UfaNet
+import io
+from .augs import get_augmentations_transformations
+import dvc.api
+
+def predict(inp, transforms, model, labels):
+        transformed_inp = transforms(image=inp)["image"]
+        with torch.no_grad():
+            prediction = torch.nn.functional.softmax(model(transformed_inp.unsqueeze(0))[0], dim=0)
+            res = {labels[i]: float(prediction[i]) for i in range(3)}
+        return (res, inp)
+
+def run_seefood():
+    labels = ["twix", "snickers", "orbit"]
+    model_info = dvc.api.read(
+        str("runs/best.ckpt"),
+        repo="https://github.com/rw404/seefood",
+        mode="rb",
+    )
+    _, transforms = get_augmentations_transformations()
+    model = UfaNet.load_from_checkpoint(io.BytesIO(model_info))
+
+
+    demo = gr.Interface(fn=lambda inp: predict(inp, transforms, model, labels),
+                inputs=gr.Image(type='numpy'),
+                outputs=[gr.Label(num_top_classes=3), 'image'])
+    demo.launch(share=True)
+    
+
